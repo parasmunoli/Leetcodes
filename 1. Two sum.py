64d47173f7da9f17717209f03ba450494e42aadd
Paras Munoli <parasmunoli@gmail.com>
2023-12-18 22:48:37 +0530

    Initial commit

---
+++ 1. Two sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums, target):
+        lookup = {}
+        for i, num in enumerate(nums):
+            if target - num in lookup:
+                return [lookup[target - num], i]
+            lookup[num] = i
+        return []
