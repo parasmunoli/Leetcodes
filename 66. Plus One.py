5ca3dc780d88b9b9aa7e79a52ef82e0b4407fe6d
Paras Munoli <parasmunoli@gmail.com>
2024-03-23 20:19:32 +0530

    solved 66

---
+++ 66. Plus One.py
@@ -0,0 +1,28 @@
+def plusOne(digits):
+    for idx in range(len(digits)-1, -1, -1):
+        if digits[idx] != 9:
+            digits[idx] += 1
+            break
+        else:
+            digits[idx] = 0
+        if digits[0] == 0:
+            digits.insert(0, 1)
+    return digits
+        
+digits = [1,2,3]
+print(plusOne(digits))
+
+#you can also use
+"""
+def plusOne(self, digits: List[int]) -> List[int]:
+        count = 0
+        for i in digits:
+            count = (count*10) + i
+        count += 1
+        li =[]
+        while(count!=0):
+            li.append(count%10)
+            count = int(count/10)
+        li.reverse()
+        return li
+"""
