f9a7d0b102130b53432a5d16b545e9b1e02b2b08
Paras Munoli <parasmunoli@gmail.com>
2024-03-25 18:59:31 +0530

    solved 121

---
+++ 121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,15 @@
+def maxProfit(prices):
+    left = 0 #Buy
+    right = 1 #Sell
+    max_profit = 0
+    while right < len(prices):
+        currentProfit = prices[right] - prices[left] #our current Profit
+        if prices[left] < prices[right]:
+            max_profit =max(currentProfit,max_profit)
+        else:
+            left = right
+        right += 1
+    return max_profit
+
+prices = [7,6,4,3,1]
+print(maxProfit(prices))
