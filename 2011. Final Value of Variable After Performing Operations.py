9d4975c3d81f4375921a62c9327a73424ce36e21
Paras Munoli <parasmunoli@gmail.com>
2024-04-11 20:00:57 +0530

    solved 2011

---
+++ 2011. Final Value of Variable After Performing Operations.py
@@ -0,0 +1,10 @@
+def finalValueAfterOperations(operations):
+    x = 0
+    for i in operations:
+        if i == '--X' or i == 'X--':
+            x -= 1
+        elif i == '++X' or i == 'X++':
+            x += 1
+    return x
+operations = ["--X","X++","X++"]
+print(finalValueAfterOperations(operations))
