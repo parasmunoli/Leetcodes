ebb2e0419e68ee75d22b98bdd832e53a7f2944a7
Paras Munoli <parasmunoli@gmail.com>
2025-05-19 22:02:26 +0530

    Solved Problem 3024

---
+++ 3024. Type of Triangle.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def triangleType(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        a, b, c = sorted(nums)
+
+        if a + b <= c:
+            return "none"
+
+        if a == b == c:
+            return "equilateral"
+        elif a == b or b == c or a == c:
+            return "isosceles"
+        else:
+            return "scalene"
