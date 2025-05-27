4243446c0fd77e0fdcb51905a98c6f549a00964d
Paras Munoli <parasmunoli@gmail.com>
2025-05-27 10:58:12 +0530

    Solved Problem 18

--- 18. 4Sum.py
+++ 18. 4Sum.py
@@ -11,10 +11,10 @@ class Solution(object):
 
         for i in range(n):
             if i > 0 and nums[i] == nums[i - 1]:
-                continue  # skip duplicates for i
+                continue
             for j in range(i + 1, n):
                 if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue  # skip duplicates for j
+                    continue
 
                 left, right = j + 1, n - 1
 
@@ -25,7 +25,6 @@ class Solution(object):
                         result.append([nums[i], nums[j], nums[left], nums[right]])
                         left += 1
                         right -= 1
-                        # skip duplicates
                         while left < right and nums[left] == nums[left - 1]:
                             left += 1
                         while left < right and nums[right] == nums[right + 1]:
