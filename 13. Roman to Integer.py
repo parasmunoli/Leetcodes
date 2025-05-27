3f3675fcf19fab4233b1a315931d2668b092438d
Paras Munoli <parasmunoli@gmail.com>
2025-05-27 11:04:19 +0530

    Solved Problem 13

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        total = 0
+        prev_value = 0
+
+        for char in reversed(s):
+            curr_value = roman_map[char]
+            if curr_value < prev_value:
+                total -= curr_value
+            else:
+                total += curr_value
+                prev_value = curr_value
+
+        return total
