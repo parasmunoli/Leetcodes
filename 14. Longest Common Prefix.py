1ec90740792f018f26cf7c25e545dcbcd910ef27
Paras Munoli <parasmunoli@gmail.com>
2025-05-27 11:04:58 +0530

    Solved Problem 14

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+
+        for word in strs[1:]:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+
+        return prefix
