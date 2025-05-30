357fc340d9ae491651e887706c3b7450ae733601
Paras Munoli <parasmunoli@gmail.com>
2025-05-30 08:20:07 +0530

    Solved Problem 40

---
+++ 40. Combination Sum II.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        results = []
+        candidates.sort()
+
+        def backtrack(start, target, path):
+            if target == 0:
+                results.append(path[:])
+                return
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                if candidates[i] > target:
+                    break
+                backtrack(i + 1, target - candidates[i], path + [candidates[i]])
+
+        backtrack(0, target, [])
+        return results
