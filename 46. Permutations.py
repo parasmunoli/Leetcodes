283588790c506d51c4071ce958185bcaf272e2b7
Paras Munoli <parasmunoli@gmail.com>
2025-05-31 11:16:54 +0530

    Solved Problem 46

---
+++ 46. Permutations.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+
+        def backtrack(path, used):
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+            for i in range(len(nums)):
+                if not used[i]:
+                    used[i] = True
+                    path.append(nums[i])
+                    backtrack(path, used)
+                    path.pop()
+                    used[i] = False
+
+        backtrack([], [False] * len(nums))
+        return result
