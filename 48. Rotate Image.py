77e9ad588df5b4eef988a453b2bf69460d978143
Paras Munoli <parasmunoli@gmail.com>
2025-06-02 21:56:38 +0530

    Solved Problem 48

---
+++ 48. Rotate Image.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+
+        # Transpose
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Reverse
+        for row in matrix:
+            row.reverse()
