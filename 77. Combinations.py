8ba34f1dfbaa4986e3f9704d6e892608d4be5b51
Paras Munoli <parasmunoli@gmail.com>
2025-06-07 19:28:48 +0530

    Solved Problem 77

---
+++ 77. Combinations.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        result = []
+
+        def backtrack(start, path):
+            if len(path) == k:
+                result.append(path[:])
+                return
+
+            for i in range(start, n + 1):
+                path.append(i)
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(1, [])
+        return result
