520f27c6815b0bd4521024fe8583a154cf9a8144
Paras Munoli <parasmunoli@gmail.com>
2025-06-08 10:11:32 +0530

    Solved Problem 386

---
+++ 386. Lexicographical Numbers.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lexicalOrder(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        result = []
+        curr = 1
+        for _ in range(n):
+            result.append(curr)
+            if curr * 10 <= n:
+                curr *= 10
+            else:
+                while curr % 10 == 9 or curr + 1 > n:
+                    curr //= 10
+                curr += 1
+        return result
